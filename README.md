5547d9f2f557a792d56f83d861dfe8d2c0fbc69d
Pol <36154612+PolRecasensSarra@users.noreply.github.com>
2018-03-04 16:20:15 +0100

    Added Red square movement and a green bullet
    Updated code

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # RedSquareBuild
 This is a build
+
+v0.4
+Added keyboard input:
+* UP: Up Arrow
+* Down: Down Arrow
+* Right: Right Arrow
+* Left: Left Arrow
+
+Added green bullet
+*Shoot: Space bar
--- RedSquare/Redsquare/main.cpp
+++ RedSquare/Redsquare/main.cpp
@@ -6,6 +6,7 @@
 using namespace std;
 int main(int argv, char *argc[])
 {
+	int op1 = 0, op2 = 0;
 	SDL_Window *window = nullptr;
 	SDL_Renderer *renderer = nullptr;
 	if (SDL_Init(SDL_INIT_VIDEO) < 0)
@@ -24,16 +25,22 @@ int main(int argv, char *argc[])
 		else
 		{
 			SDL_Rect rectangle;
-			rectangle.x = 240;
-			rectangle.y = 180;
+			rectangle.x = 100;
+			rectangle.y = 100;
 			rectangle.w = 120;
 			rectangle.h = 120;
+			SDL_Rect bullet;
+			bullet.x = rectangle.x + 120;
+			bullet.y = rectangle.y - 60;
+			bullet.w = 35;
+			bullet.h = 10;
 
 			renderer = SDL_CreateRenderer(window, -1, 0);
 			SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
 			SDL_RenderClear(renderer);
-			bool loop = true;
 
+			bool loop = true;
+			bool bulletaux = NULL;
 			while (loop)
 			{
 				SDL_Event event;
@@ -43,21 +50,48 @@ int main(int argv, char *argc[])
 					{
 					case SDL_QUIT:
 						loop = false;
+						break;
+					case SDL_KEYDOWN:
+						switch (event.key.keysym.sym) {
+						case SDLK_SPACE:
+							bulletaux = true;
+							bullet.x = rectangle.x + 120;
+							bullet.y = rectangle.y +50;
+							break;
+						case SDLK_UP:
+							rectangle.y -= 11;
+							break;
+						case SDLK_DOWN:
+							rectangle.y += 11;
+							break;
+						case SDLK_RIGHT:
+							rectangle.x += 11;
+							break;
+						case SDLK_LEFT:
+							rectangle.x -= 11;
+							break;
+
+						}
 					}
+
 				}
 				SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
 				SDL_RenderFillRect(renderer, &rectangle);
 				SDL_RenderPresent(renderer);
-
+				if (bulletaux) {
+					SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+					SDL_RenderFillRect(renderer, &bullet);
+					SDL_RenderPresent(renderer);
+					bullet.x += 10;
+					if (bullet.x >= 640) {
+						bulletaux = false;
+					}
+					SDL_Delay(2);
+				}
 				SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
 				SDL_RenderClear(renderer);
-				if (rectangle.x <= 360) {
-					rectangle.x++;
-					if (rectangle.x == 360) {
-						rectangle.x = 0;
-					}
-				}
-				SDL_Delay(5);
+
+				
 			}
 		}
 	}
