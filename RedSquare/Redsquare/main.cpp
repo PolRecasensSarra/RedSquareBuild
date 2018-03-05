cf6a60e5ba0dd6f8ffbeab2bf13b3724560d074b
Pol <36154612+PolRecasensSarra@users.noreply.github.com>
2018-03-05 10:05:28 +0100

    Updated Main
    Added bullet array.

--- RedSquare/Redsquare/main.cpp
+++ RedSquare/Redsquare/main.cpp
@@ -29,18 +29,21 @@ int main(int argv, char *argc[])
 			rectangle.y = 100;
 			rectangle.w = 120;
 			rectangle.h = 120;
-			SDL_Rect bullet;
-			bullet.x = rectangle.x + 120;
-			bullet.y = rectangle.y - 60;
-			bullet.w = 35;
-			bullet.h = 10;
 
+			SDL_Rect bullet[5];
+			for (int i = 0; i < 5; i++) {
+				bullet[i].x = NULL;
+				bullet[i].y = NULL;
+				bullet[i].w = NULL;
+				bullet[i].h = NULL;
+			}
 			renderer = SDL_CreateRenderer(window, -1, 0);
 			SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
 			SDL_RenderClear(renderer);
 
+			int bulletnum = 0;
 			bool loop = true;
-			bool bulletaux = NULL;
+			bool bulletaux = false;
 			while (loop)
 			{
 				SDL_Event event;
@@ -55,21 +58,69 @@ int main(int argv, char *argc[])
 						switch (event.key.keysym.sym) {
 						case SDLK_SPACE:
 							bulletaux = true;
-							bullet.x = rectangle.x + 120;
-							bullet.y = rectangle.y +50;
+							switch (bulletnum) {
+							case 0:
+								bullet[0].x = rectangle.x + 120;
+								bullet[0].y = rectangle.y +50;
+								bullet[0].w = 35;
+								bullet[0].h = 10;
+								bulletnum++;
+								break;
+							case 1:
+								bullet[1].x = rectangle.x + 120;
+								bullet[1].y = rectangle.y + 50;
+								bullet[1].w = 35;
+								bullet[1].h = 10;
+								bulletnum++;
+								break;
+							case 2:
+								bullet[2].x = rectangle.x + 120;
+								bullet[2].y = rectangle.y + 50;
+								bullet[2].w = 35;
+								bullet[2].h = 10;
+								bulletnum++;
+								break;
+							case 3:
+								bullet[3].x = rectangle.x + 120;
+								bullet[3].y = rectangle.y + 50;
+								bullet[3].w = 35;
+								bullet[3].h = 10;
+								bulletnum++;
+								break;
+							case 4:
+								bullet[4].x = rectangle.x + 120;
+								bullet[4].y = rectangle.y + 50;
+								bullet[4].w = 35;
+								bullet[4].h = 10;
+								bulletnum=0;
+								break;
+						}
 							break;
 						case SDLK_UP:
 							rectangle.y -= 11;
+							if (rectangle.y <= 0) {
+								rectangle.y = 0;
+							}
 							break;
 						case SDLK_DOWN:
 							rectangle.y += 11;
+							if (rectangle.y > 360) {
+								rectangle.y = 360;
+							}	
 							break;
 						case SDLK_RIGHT:
 							rectangle.x += 11;
+							if (rectangle.x > 520) {
+								rectangle.x = 520;
+							}
 							break;
 						case SDLK_LEFT:
 							rectangle.x -= 11;
+							if (rectangle.x < 0) {
+								rectangle.x = 0;
+							}
 							break;
+							
 
 						}
 					}
@@ -78,16 +129,17 @@ int main(int argv, char *argc[])
 				SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
 				SDL_RenderFillRect(renderer, &rectangle);
 				SDL_RenderPresent(renderer);
+
 				if (bulletaux) {
-					SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
-					SDL_RenderFillRect(renderer, &bullet);
-					SDL_RenderPresent(renderer);
-					bullet.x += 10;
-					if (bullet.x >= 640) {
-						bulletaux = false;
+					for (int i = 0; i < 5; i++) {
+						SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+						SDL_RenderFillRect(renderer, &bullet[i]);
+						SDL_RenderPresent(renderer);
+						bullet[i].x += 10;
 					}
-					SDL_Delay(2);
+					SDL_Delay(10);
 				}
+				
 				SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
 				SDL_RenderClear(renderer);
 
